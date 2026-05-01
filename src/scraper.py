48cada93bf6eda35e092311312180a8b9f086303
Leandro Bispo <leandro_reisbispo@hotmail.com>
2026-05-01 11:50:36 -0300

    feat: return structured DataFrame from extrair_detalhes

--- src/scraper.py
+++ src/scraper.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 
 def buscar_vagas(url):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WIN64) cHROME/120.0.0.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WIN64) cHROME/120.0.0.0" 
     }
     resposta = requests.get(url, headers=headers)
     soup = BeautifulSoup(resposta.text, "html.parser") #Tratando url (HTML)
 
     return soup
+
 
 def extrair_vagas(soup):
 
@@ -17,26 +18,51 @@ def extrair_vagas(soup):
     return resposta
 
 
-
 def extrair_detalhes(vagas):
+    # 1. listas vazias 
+    cargos = []
+    empresas = []
+    cidades = []
+    niveis = []
 
     for vaga in vagas:
+        # 2. extraindo dados
         cargo = vaga.find("a", class_="link-detalhes-vaga")
         texto_cargo = cargo.text.strip() if cargo else "Não informado"
+
         empresa = vaga.find("span", class_="emprVaga")
         texto_empresa = empresa.text.strip() if empresa else "Não informado"
+
         cidade = vaga.find("span", class_="local")
         texto_cidade = cidade.text.strip() if cidade else "Não informado"
+
         nivel_vaga = vaga.find("span", class_="nivelVaga")
         texto_nivel_vaga = nivel_vaga.text.strip() if nivel_vaga else "Não informado"
-        print(f"Cargo: {texto_cargo} | Empresa: {texto_empresa} | Cidade: {texto_cidade} | Nivel Vaga: { texto_nivel_vaga}")
+
+        #print(f"Cargo: {texto_cargo} | Empresa: {texto_empresa} | Cidade: {texto_cidade} | Nivel Vaga: { texto_nivel_vaga}")
+
+        # 3. adicionando o que foi extraído nas listas
+        cargos.append(texto_cargo)
+        empresas.append(texto_empresa)
+        cidades.append(texto_cidade)
+        niveis.append(texto_nivel_vaga)
+
+        # 4. Retornar DataFrame
+
+    import pandas as pd
+    df = pd.DataFrame({
+        "cargo": cargos,
+        "empresa": empresas,
+        "cidade": cidades,
+        "nivel": niveis
+    })
+
+    return df
         
-
-
 
 
 url = "https://www.vagas.com.br/vagas-de-analista-de-dados"
 soup = buscar_vagas(url)
 vagas = extrair_vagas(soup)
-print(len(vagas))  # quantas vagas encontrou
-extrair_detalhes(vagas)
+df = extrair_detalhes(vagas)
+print(df)
